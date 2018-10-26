b3a4d8e800c2ea562175e1454e14281b260b803c
Alli Colyer <allicolyer@gmail.com>
2018-10-25 19:06:38 -0700

    make about the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   resources :rates
-  root 'static_pages#home'
+  root 'static_pages#about'
   get '/help',  to: 'static_pages#help'
-  get '/about',  to: 'static_pages#about'
+  get '/calculator',  to: 'static_pages#calculator'
   get '/contact',  to: 'static_pages#contact'
 
 end
